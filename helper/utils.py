a4da39bb32f328aaf828ce5a8918f9651e8287e0
ccaballeroh <cdch10@gmail.com>
2020-04-27 17:52:44 -0500

    added docstrings

--- helper/utils.py
+++ helper/utils.py
@@ -1,7 +1,19 @@
+"""Util functions to clean directory during processing and testing."""
+
 from pathlib import Path
 
 
 def cleaning(*, path: Path, extension: str, affix: str) -> None:
+    """Deletes all files in a Path with certain extension and name termination.
+    
+    Inputs:
+    path:       Path    - directory where to delete files
+    extension:  str     - extension of files to errase
+    affix:      str     - temination of file name to delete
+
+    Returns:
+    None
+    """
     assert path.is_dir(), f"Path: {path} does not exist"
     for filename in path.iterdir():
         if filename.suffix == extension and filename.stem.endswith(affix):
@@ -10,6 +22,7 @@ def cleaning(*, path: Path, extension: str, affix: str) -> None:
 
 
 def clean_example():
+    """Deletes all files created during processing or corpora no longer needed during experiments."""
     JSON_FOLDER = Path(r"./auxfiles/json/")
     cleaning(path=JSON_FOLDER, extension=".json", affix="trash")
 
