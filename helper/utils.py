5317b644daf71ce739564f48aa5d040add5aec8d
ccaballeroh <cdch10@gmail.com>
2020-04-28 22:27:43 -0500

    created a new clean up function

--- helper/utils.py
+++ helper/utils.py
@@ -6,7 +6,7 @@ from pathlib import Path
 def cleaning(*, path: Path, extension: str, affix: str) -> None:
     """Deletes all files in a Path with certain extension and name termination.
     
-    Inputs:
+    Parameters:
     path:       Path    - directory where to delete files
     extension:  str     - extension of files to errase
     affix:      str     - temination of file name to delete
@@ -21,11 +21,8 @@ def cleaning(*, path: Path, extension: str, affix: str) -> None:
     return None
 
 
-def clean_example():
-    """Deletes all files created during processing or corpora no longer needed during experiments."""
-    JSON_FOLDER = Path(r"./auxfiles/json/")
-    cleaning(path=JSON_FOLDER, extension=".json", affix="trash")
-
+def clean_files():
+    """Deletes all files created during processing of corpora no longer needed during experiments."""
     CORPORA = Path(r"./Corpora/Proc_Quixote")
     if CORPORA.exists():
         cleaning(path=CORPORA, extension=".txt", affix="proc")
@@ -41,5 +38,13 @@ def clean_example():
     cleaning(path=(SN_FOLDER / "Ibsen"), extension=".txt", affix="sn")
 
 
+def clean_example():
+    """Deletes all files created during teh example run in `analysis` submodule."""
+    JSON_FOLDER = Path(r"./auxfiles/json/")
+    cleaning(path=JSON_FOLDER, extension=".json", affix="trash")
+
+    clean_files()
+
+
 if __name__ == "__main__":
-    clean_example()
+    pass
