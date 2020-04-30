7b112edd3aa19d4d31b136b9c92b32d294212938
ccaballeroh <cdch10@gmail.com>
2020-04-30 14:12:56 -0500

    changed name in google drive folder

--- helper/__init__.py
+++ helper/__init__.py
@@ -5,10 +5,11 @@ __author__ = "Christian Caballero <cdch10@gmail.com>"
 from pathlib import Path
 import sys
 
+# flag to change root folder if running in colab
 IN_COLAB = "google.colab" in sys.modules
 
 if IN_COLAB:
-    ROOT = Path(r"./drive/My Drive/00Tesis/")
+    ROOT = Path(r"./drive/My Drive/Translator-Attribution/")  # Root folder in colab
     print("In colab!")
 else:
-    ROOT = Path(r".")
+    ROOT = Path(r".")  # Root folder if running locally
